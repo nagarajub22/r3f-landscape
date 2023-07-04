248fd9a3b606e55b09d9874f257212c5ce3daff5
NagaRaju <nagaraju.b22@outlook.com>
2023-07-04 22:26:10 +0530

    v1 background animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import './assets/background_steps.png';
 import { AppPageList } from './config/common';
+import { BackgroundContent } from './pages/Background';
 import { PageContent } from './pages/Content';
 import NavBar from './pages/common/NavBar';
-import { BackgroundContent } from './pages/Background';
 
 function App() {
 
@@ -14,11 +14,11 @@ function App() {
     <>
       {/* <div id="site-bg-anim"></div> */}
       <BackgroundContent/>
-      {/* <div id="site-overlay"></div>
+      <div id="site-overlay"></div>
       <div id="site-main">
         <PageContent/>
         <NavBar activePage={activePage} changePage={changePage}/>
-      </div> */}
+      </div>
     </>
   )
 }
